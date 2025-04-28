e48398d9ee532661e6d3a64997dc72a5c5003ffd
Revanth-02082006 <revanthsathya.ind@gmail.com>
2025-04-28 19:54:50 +0530

    Create Automated Email Sender.py

---
+++ Day2/Automated Email Sender.py
@@ -0,0 +1,7 @@
+import smtplib
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login("your_email@gmail.com", "your_password")
+server.sendmail("your_email@gmail.com", "receiver_email@gmail.com", "Hello from Python!")
+server.quit()
