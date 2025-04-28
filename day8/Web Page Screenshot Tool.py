c3cafd23bd2669d7744b2bcb5a098237a02db8a9
Revanth-02082006 <revanthsathya.ind@gmail.com>
2025-04-28 20:44:20 +0530

    Create Web Page Screenshot Tool.py

---
+++ day8/Web Page Screenshot Tool.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+url = "https://example.com"
+driver = webdriver.Chrome()
+driver.get(url)
+driver.save_screenshot("webpage_screenshot.png")
+driver.quit()
+
+print("Screenshot saved successfully!")
